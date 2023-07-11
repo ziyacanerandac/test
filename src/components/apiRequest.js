7e88eb71459e0015bdd3391ec27d3956f46f3511
Ziyacan Erandaç <ziyacan.erandac@inavitas.com>
2023-07-11 19:33:29 +0300

    add-new thins

--- src/components/apiRequest.js
+++ src/components/apiRequest.js
@@ -3,19 +3,24 @@ import axios from 'axios';
 
 const ApiRequest = () => {
     const [persons, setPersons] = useState([])
+   
+    const headers = {
+        "Content-Type": "application/json"
+      };
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts`,{headers})
         .then(res => {
-          const personData = res.data;
+            console.log(res);
+          const personData = res.results;
           setPersons(personData);
         })
-    }, [])
+    }, [3000])
     
   return (
     <div>
-        {persons.map((person) => (
-            <div key={person.id}>
-                <p>{person.name}-{person.website}</p>
+        {persons.map((s) => (
+            <div key={s.id}>
+                <p>{s.business_status}-{s.website}</p>
             </div>
         ))}
     </div>
